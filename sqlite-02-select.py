2a8dd7d1d34969e8d9e24a5ed8557d16c255b718
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-08 08:26:18 -0300

    up

--- sqlite-02-select.py
+++ sqlite-02-select.py
@@ -15,24 +15,25 @@ import sqlite3 as db
 db1 = db.connect("agenda.db")
 
 
-# antes devemos criar o primeiro o objeto cursor
+# antes devemos criar o primeiro o objeto cursor.
 # que vai receber o banco e assim podemos executar os comando SQL (criar tabela, inserir, excluir, etc)
 cursor = db1.cursor()
 
 
 # para visualizar os dados da tabela
 # execute um SELECT para o cursor
-# print o método fetchall() (fetch -> buscar)
-cursor.execute("SELECT * FROM pessoas")
-print(cursor.fetchall()) 
-print(type(cursor.fetchall())) # retorna uma lista 
+cursor.execute("SELECT * FROM pessoas") # * -> todos os campos
+
+# print o método fetchall() (fetch -> buscar) que retorna uma lista com os dados
+print(cursor.fetchall())   # retorna uma lista com os dados da tabela pessoas
+print(type(cursor.fetchall())) # retorna uma lista de tuplas (tupla é imutável)
 
 
 # imprimir em linha
 cursor.execute("SELECT * FROM pessoas")
 tabela = cursor.fetchall()
-for i in tabela:
-  print(i)
+for i in tabela: # para cada linha na tabela e imprima a linha
+  print(i) # imprime a linha da tabela pessoas (tupla)
 
 db1.close()
 # shift + alt + a cria comentários em múltiplas linhas no repl.it
