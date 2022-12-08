918b160d6de74d34604143ca7b2a9f71b9ef9725
Michel <106674127+MichelZero@users.noreply.github.com>
2022-12-08 08:31:18 -0300

    up

--- sqlite-03-delete.py
+++ sqlite-03-delete.py
@@ -14,7 +14,25 @@ import sqlite3 as db
 # criar o banco ou conecta o banco
 db1 = db.connect("agenda.db")
 
-
 # antes devemos criar o primeiro o objeto cursor.
 # que vai receber o banco e assim podemos executar os comando SQL (criar tabela, inserir, excluir, etc)
 cursor = db1.cursor()
+
+# deletar um registro
+try:
+  # correto 
+  cursor.execute("DELETE from pessoas WHERE idade = 9") 
+  db1.commit()
+  
+  # errado
+  cursor.execute("DELETE from pessoas WHERE idade2 = 9") 
+  db1.commit()
+  #  gerar o erro 'no such column: idade2' 
+  
+  
+  cursor.execute("SELECT * FROM pessoas") # * -> todos os campos
+  print(cursor.fetchall()) 
+  db1.close() # fecha o banco
+  print("dados removidos!")
+except Exception as e: # caso ocorra um erro imprima o erro
+  print(str(e))  # imprime o erro gerado
